7dcf479d07a76669a5642c6eb96a4288d985c4de
Antonio Marotta <marotta.anto@gmail.com>
2024-06-18 10:42:40 +0200

    chore: bump

--- src/hooks/useBoxStyle.ts
+++ src/hooks/useBoxStyle.ts
@@ -1,6 +1,6 @@
-import type { CSSProperties } from "react";
 import type { IPriceBlockBox } from "../components/types";
 import { getProportionedSize } from "./get-proportioned-size";
+import type { CSSProperties } from "react";
 
 interface IUseBoxStyleProps {
   box: IPriceBlockBox | undefined;
