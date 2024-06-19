663d5c68e46eba045793b0cef046147594411db0
Aldo Vandus <aldovandus@gmail.com>
2024-06-19 13:14:54 +0200

    fix: formatted currency proportions

--- src/components/separate-number-formatted/useFormattedCurrencyStyle.ts
+++ src/components/separate-number-formatted/useFormattedCurrencyStyle.ts
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import type { CSSProperties } from "react";
 import { PriceFormatType } from "../types";
+import { getProportionedSize } from "../../hooks/get-proportioned-size";
 
 type Props = {
   className?: string;
@@ -13,22 +14,25 @@ const useFormattedCurrencyStyle = ({ className, fontSize, type }: Props) => {
     return {
       containerClass: className,
       decimalClass: "flex gap-2",
-      integerStyle: { fontSize: `${fontSize}px` },
-      decimalStyle: { fontSize: `${fontSize}px` },
-      currencyStyle: { fontSize: `${fontSize}px` }
+      integerStyle: { fontSize: `${getProportionedSize(fontSize)}` },
+      decimalStyle: { fontSize: `${getProportionedSize(fontSize)}` },
+      currencyStyle: { fontSize: `${getProportionedSize(fontSize)}` }
     };
 
   const isType1 = type == PriceFormatType.TYPE1;
-  //const isType2 = type == PriceFormatType.TYPE2;
+  const isType2 = type == PriceFormatType.TYPE2;
   const isType3 = type == PriceFormatType.TYPE3;
   const isType4 = type == PriceFormatType.TYPE4;
 
   const containerClass = classNames(className, { "items-end": isType1 }, { "items-center": isType4 || isType3 });
   const decimalClass = classNames("flex gap-2", { "flex-col-reverse  flex-col": isType3 });
-  const integerStyle: CSSProperties = { fontSize: `${fontSize}px`, lineHeight: isType3 ? `calc(${fontSize}px - 10%)` : undefined };
-  const decimalContainerStyle: CSSProperties = { lineHeight: isType3 ? `calc(${fontSize}px - 60%)` : undefined };
-  const currencyStyle: CSSProperties = { fontSize: `calc(${fontSize}px - 60%)` };
-  const decimalStyle = { fontSize: `calc(${fontSize}px - 45%)` };
+  const integerStyle: CSSProperties = {
+    fontSize: `${getProportionedSize(fontSize)}`,
+    lineHeight: isType3 ? `calc(${getProportionedSize(fontSize)} - 10%)` : undefined
+  };
+  const decimalContainerStyle: CSSProperties = { lineHeight: isType3 ? `calc(${getProportionedSize(fontSize)} - 60%)` : undefined };
+  const currencyStyle: CSSProperties = { fontSize: `calc(${getProportionedSize(fontSize)} - 60%)` };
+  const decimalStyle = { fontSize: `calc(${getProportionedSize(fontSize)} - 45%)` };
 
   return {
     containerClass,
