f45906d8f3336b4c05e46a85321f23a02cc4274b
Aldo Vandus <aldovandus@gmail.com>
2024-06-21 10:44:42 +0200

    chore: add tailwind to postcss plugins viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import dts from "vite-plugin-dts";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()]
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
